c4b206be46d0779e4878d5e33c2958a3e3a4a33f
Spyder-code <luaysyauqillah@gmail.com>
2022-10-15 14:40:51 +0700

    update fltpicker

--- resources/views/layouts/newuser.blade.php
+++ resources/views/layouts/newuser.blade.php
@@ -25,6 +25,7 @@
     <link rel="stylesheet" href="{{ asset('social') }}/assets/css/app.css">
     <!-- Google Web Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
+    @yield('style')
     <style>
         .loading {
             z-index: 9999;
