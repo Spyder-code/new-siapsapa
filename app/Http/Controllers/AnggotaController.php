42fde8197cef3bab4486231b7a4b58ddbd9e9520
Spyder-code <luaysyauqillah@gmail.com>
2024-09-03 13:31:02 +0700

    edit kwarnas

--- app/Http/Controllers/AnggotaController.php
+++ app/Http/Controllers/AnggotaController.php
@@ -154,7 +154,7 @@ class AnggotaController extends Controller
         }else{
             $user = Auth::user();
             $role = $user->role;
-            if($role=='admin')
+            if($role=='admin' || $role=='kwarnas')
                 $id_wilayah = 'all';
             if($role=='kwarda')
                 $id_wilayah = $user->anggota->provinsi;
